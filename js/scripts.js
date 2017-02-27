46d5c11fe989254f428f23b18d68a41032072c50
Karolina <szefu6@wp.pl>
2017-02-27 14:13:55 +0100

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// plik scripts.js
+
+/* 
+	komentarz blokowy 
+	color changes in css; 
+*/
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name); 
+//alert('test') alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
